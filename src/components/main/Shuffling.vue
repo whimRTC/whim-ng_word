d77b044e5a295e707acb4bb207f6ef5cb34e683e
Odagawa <xtctl.1234@gmail.com>
2020-06-23 12:39:44 +0900

    refactor

--- src/components/main/Shuffling.vue
+++ src/components/main/Shuffling.vue
@@ -8,37 +8,4 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-.select {
-  margin: 10px;
-}
-
-.icon {
-  width: 40px;
-  height: 40px;
-  vertical-align: middle;
-}
-
-.select_text {
-  width: 200px;
-  display: inline-block;
-  vertical-align: middle;
-}
-
-.fuwatto_btn_yellow {
-  display: block;
-  background-color: #000000;
-  color: #fff;
-  padding: 0.8em;
-  text-decoration: none;
-  border-radius: 4px;
-  transition: 0.3s ease-out;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 140px;
-  text-align: center; /*一応BOX内の文字も中央寄せ*/
-}
-</style>
+<style lang="scss" scoped></style>
